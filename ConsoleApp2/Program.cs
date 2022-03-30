012174da9f80d8ea5332bf0b9cbccfe91fb72915
baires1971 <baires1971@hotmail.com>
2022-03-30 20:18:10 -0300

    02-Encapsulamiento. al declarar publico un metodo de clase cuadrado, se puede acceder a el desde fuera de la clase

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,23 +8,24 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            /*  declaramos una variable figura como un objeto de la clase cuadrado
-                luego creamos el objeto o instanciamos la clase con la orden new
-                el metodo calculararea() no puede ser accedido con la declaracion "." del
-                objeto figura 	
-
-           */
+            /* cuando declaramos como publico el metodo calculararea, este se puede usar fuera del ambito de la clase cuadrado
+               en cambio la propiedad lado no puede ser accedida fuera de la clase, en virtud a la propiedad de encapsulamiento 
+               que tienen los objetos.
+            */
             Cuadrado figura;
             figura = new Cuadrado();
-            Console.WriteLine("el area del cuadrado es: {0}", figura);
+            Console.WriteLine("el area del cuadrado es: {0}", figura.CalcularArea());
+            Console.ReadKey();
         }
     }
 
     class Cuadrado
     {
-        double lado = 10; // propiedad de la clase cuadrado en este caso privado por defecto
+        private double lado = 10; // cuando declaramos private a una propiedad o metodo este no puede ser accedido desde 
+                                  // fuera de la clase contenedora. Si no especificamos nada por defecto C# toma como 
+                                  // privadas todas las propiedades y metodos, siguiendo el concepto de encapsulamiento	
 
-        double CalcularArea(double lado) // metodo de la clase cuadrado que calcula su area
+        public double CalcularArea()
         {
             return lado * lado;
         }
@@ -34,6 +35,8 @@ namespace ConsoleApp2
 
 
 }
+
+
 
 
 
