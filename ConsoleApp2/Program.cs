628bd0d4df78366fc5d23096253f897db8c94329
baires1971 <baires1971@hotmail.com>
2022-03-30 20:09:48 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  declaramos una variable figura como un objeto de la clase cuadrado
+                luego creamos el objeto o instanciamos la clase con la orden new
+                el metodo calculararea() no puede ser accedido con la declaracion "." del
+                objeto figura 	
+
+           */
+            Cuadrado figura;
+            figura = new Cuadrado();
+            Console.WriteLine("el area del cuadrado es: {0}", figura);
+        }
+    }
+
+    class Cuadrado
+    {
+        double lado = 10; // propiedad de la clase cuadrado
+
+        double CalcularArea(double lado) // metodo de la clase cuadrado que calcula su area
+        {
+            return lado * lado;
+        }
+
+    }
+
+
+
+}
+
+
+
+
+
+
