25d2dc591b4bcd55cfbe22a830f770fa8e11e3f1
baires1971 <baires1971@hotmail.com>
2022-04-05 20:19:05 -0300

    17-cuando una clase hereda de varias interfaces, pude ocurrir que dos o mas posean un metodo que tenga el mismo nombre

--- ConsoleApp2/Directivo.cs
+++ ConsoleApp2/Directivo.cs
@@ -8,25 +8,32 @@ namespace ConsoleApp2
 {
 
     interface IAdministrativo //interface que define el metodo usarexcel que debe obligatoriamente ser implementado por la clase
-    {                         //que utilice esta interface, siempre es publico
+    {                         //que utilice esta interface
         void UsarExcel();
-        
+        void UsarLogica();
     }
 
-    interface IDocente 
+    interface IDocente
     {
         void UsarPedagogia();
-      
+        void UsarLogica();
     }
-    internal class Directivo:Persona, IAdministrativo, IDocente  //siempre que una clase herede, primero se ponde la clase y despues recien las interfaces
+    internal class Directivo : Persona, IAdministrativo, IDocente  //siempre que una clase herede, primero se ponde la clase y despues recien las interfaces
     {
-        public Directivo(string nombre, int _dni, double _altura, double _peso, DateTime nacimiento) : base(nombre, _dni, _altura, _peso, nacimiento) 
+        public Directivo(string nombre, int _dni, double _altura, double _peso, DateTime nacimiento) : base(nombre, _dni, _altura, _peso, nacimiento)
         {
-        
+
         }
-    
-        public void UsarExcel() { Console.WriteLine("Utilizo planillas de calculo");}
+        //Tenemos una ambiguedad en el metodo usarlogica(), a cual interface corresponde?, pues a ambas, y esto no puede ser
+        //public void UsarLogica() { Console.WriteLine("Utilizo la logica"); }
+        public void UsarExcel() { Console.WriteLine("Utilizo planillas de calculo"); }
         public void UsarPedagogia() { Console.WriteLine("Utilizo pedagogia"); }
-        
+        /*
+         *Para solucionar la ambiguedad se escribe el nombre del metodo sin el modificador public y declarando a que interface corresponde
+         *el problema es que al no ser public queda encapsulado como private por defecto y no se puede acceder desde fuera de la clase
+         */
+        void IAdministrativo.UsarLogica() { Console.WriteLine("Utilizo la logica"); }
+        void IDocente.UsarLogica() { Console.WriteLine("Utilizo otro tipo de logica"); }
+
     }
 }
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,21 +8,33 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            /* una clase puede ser obligada a tener un comportamiento determinado mediante la implementacion de interfaces
-             * Básicamente nos permiten definir un "contrato" sobre el que podemos estar seguros de que, las clases que las implementen, lo van a cumplir.
-             * las intefaces declaran solamente propiedades o metodos, por definicion son siempre publicos. pero su implementacion y codificacion
-             * queda bajo la responsabilidad de la clase que hereda la interface. es decir la interface nos dice que metodos tiene que tener la clase
-             * pero no impone como hacerlos. Siempre la clase hereda de una clase padre y despues de una, dos o mas interfaces.
-             * 
-             */
+            /* cuando una clase hereda de varias interfaces, pude ocurrir que dos o mas posean un metodo que tenga el mismo nombre, numero de parametros
+             * y devuelva el mismo tipo de valor. En ese caso se produce una ambiguedad, cuando implementamos el metodo a que interface corresponde?
+             * el compilador no arroja error, debemos implementar dos metodos uno para cada interface, pero de manera privada, lo cual no podemos
+             * acceder a estos metodos desde fuera de la clase. para solucionar este problema tenemos que utilizar el principio de sustitucion.
+              
+           */
 
             Directivo Miguel = new Directivo("Miguel", 23456921, 1.73d, 78d, DateTime.Parse("12/07/2000"));
             Miguel.UsarPedagogia();  //metodo heredado de la interface IDocente
             Miguel.UsarExcel();      //metodo heredado de la interface IAdministrativo  
+            //Miguel.UsarLogica();  //esta llamada esta mal, porque no se sabe a cual interface se hace referencia
+            //debemos utilizar el principio de sustitucion
+            IAdministrativo administrativo = Miguel;
+            administrativo.UsarLogica(); //el metodo usarlogica() pertenece a la interface IAdministrativo
+            IDocente docente = Miguel;
+            docente.UsarLogica();  // el metodo usarlogica() pertenece a la interface IDocente
+
             Console.WriteLine(Miguel.Edad); //propiedad heredada de la clase persona
         }
     }
 }
+
+
+
+
+
+
 
 
 
