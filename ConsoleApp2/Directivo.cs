5e1447e8694587f59e6dd9db81a024afcb99c54f
baires1971 <baires1971@hotmail.com>
2022-04-05 20:10:00 -0300

    16- implementacion de interfaces

---
+++ ConsoleApp2/Directivo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+    interface IAdministrativo //interface que define el metodo usarexcel que debe obligatoriamente ser implementado por la clase
+    {                         //que utilice esta interface, siempre es publico
+        void UsarExcel();
+        
+    }
+
+    interface IDocente 
+    {
+        void UsarPedagogia();
+      
+    }
+    internal class Directivo:Persona, IAdministrativo, IDocente  //siempre que una clase herede, primero se ponde la clase y despues recien las interfaces
+    {
+        public Directivo(string nombre, int _dni, double _altura, double _peso, DateTime nacimiento) : base(nombre, _dni, _altura, _peso, nacimiento) 
+        {
+        
+        }
+    
+        public void UsarExcel() { Console.WriteLine("Utilizo planillas de calculo");}
+        public void UsarPedagogia() { Console.WriteLine("Utilizo pedagogia"); }
+        
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,25 +8,18 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            /* podemos escribir un metodo de una clase con el modificador public, private y protected
-             * protected hace que el metodo solo sea accesible por la clase donde lo declaramos y por las clases hijas de esta. no pudiendose
-             * llamar desde otras clases, esta en una posicion intermedia entre private y public
-              
-           */
-            
-            Profesor Juan = new Profesor("Juan", 27869324, 1.75d, 76d,DateTime.Parse("12/03/2000"));
-            Alumno Maria = new Alumno("Maria", 30124665, 1.70d, 65d, DateTime.Parse("05/04/2000"));
-            Profesor Jorge = Juan; //creamos dos referencias o punteros al mismo objeto
-            Persona[] personas = new Persona[3];
-            personas[0] = Juan;  //el metodo firmar() implementado en juan es distinto al firmar() de persona, entonces como el arreglo es del tipo persona
-                                 //solo son accesibles las propiedades y metodos de esta clase y vemos el metodo firmar de la clase persona
-            personas[1] = Maria; //como en alumno sobreescribimos el metodo firmar, el compilador toma que modificamos el metodo de la clase persona y al acceder al nuevo
-                                 //metodo se comporta de manera distinta
-            personas[2] = Jorge;
-            for(int i = 0; i < personas.Length; i++) { personas[i].Firmar(); }
-            personas[1].Caminar; //si intentamos acceder al metodo caminar() de la clase persona no podemos porque es del tipo protected
+            /* una clase puede ser obligada a tener un comportamiento determinado mediante la implementacion de interfaces
+             * Básicamente nos permiten definir un "contrato" sobre el que podemos estar seguros de que, las clases que las implementen, lo van a cumplir.
+             * las intefaces declaran solamente propiedades o metodos, por definicion son siempre publicos. pero su implementacion y codificacion
+             * queda bajo la responsabilidad de la clase que hereda la interface. es decir la interface nos dice que metodos tiene que tener la clase
+             * pero no impone como hacerlos. Siempre la clase hereda de una clase padre y despues de una, dos o mas interfaces.
+             * 
+             */
 
-
+            Directivo Miguel = new Directivo("Miguel", 23456921, 1.73d, 78d, DateTime.Parse("12/07/2000"));
+            Miguel.UsarPedagogia();  //metodo heredado de la interface IDocente
+            Miguel.UsarExcel();      //metodo heredado de la interface IAdministrativo  
+            Console.WriteLine(Miguel.Edad); //propiedad heredada de la clase persona
         }
     }
 }
