3d8d9575af8391739b3b428a6fa9ca2eecf2304d
baires1971 <baires1971@hotmail.com>
2022-04-06 17:23:47 -0300

    21- tipos enumerados

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,67 +4,39 @@
 
 namespace ConsoleApp2
 {
+    //declaramos un enumerado con los dias de la semana
+    enum Dias { Lunes, Martes, Miercoles, Jueves, Viernes, Sabado, Domingo}
+
+    // declaramos un enumerado y le asignamos un valor a cada constante
+    enum NivelGlucosa { Bajo = 100, Medio = 150, Alto = 200, MuyAlto = 300 }
     class Program
     {
         static void Main(string[] args)
         {
-            /* Cuando trabajamos con estructuras, estas se almacenan en la pila o stack, como si fuera una variable primitiva o por valor
-             * en cambio las clases se almacenan en el heap y se accede a ellas mediante referencias. las estructuras no tienen constructores
-             *por defecto, el compilador no inicializa las propiedades hay que crear un constructor especifico. 
-             *las estructuras no heredan de otras clases, pero si pueden implementar interfaces, son selladas (sealed). se utilizan cuando
-             *hay que trabajar con grandes cantidades de datos en memoria, con distintos tipos primitivos, en virtud que son mas rapidos
-             *que las clases
+            /* Los tipos enumerados son un conjunto de constantes con nombres, sirven para manejar y manipular valores fijos o constantes
+             * en un programa. Los tipos enumerados casi siempre se declaran a nivel o dentro de los nameespace para ser utilizados por 
+             * varias clases. El compilador le asigna a cada constante de un tipo enumerado un valor de 0 ...n  
+             * 
            */
-            Auxiliar auxiliar = new Auxiliar();
-            Auxiliar1 auxiliar1 = new Auxiliar1();
-            auxiliar.SetHoras(24);
-            auxiliar.SetDias(8);
-            auxiliar1.SetHoras(24);
-            auxiliar1.SetDias(8);
-            Console.WriteLine("El auxiliar trabajo {0} horas durante {1} dias", auxiliar.GetHoras(), auxiliar.GetDias());
-            Console.WriteLine("El auxiliar1 trabajo {0} horas durante {1} dias", auxiliar1.GetHoras(), auxiliar1.GetDias());
-            Console.WriteLine("================================================================");
-            auxiliar.IncrementarHoras(auxiliar, 100);
-            auxiliar1.IncrementarHoras(auxiliar1 , 100);
-            Console.WriteLine("Las horas modificadas en el objeto de la clase Auxiliar es {0}", auxiliar.GetHoras());
-            Console.WriteLine("Las horas modificadas en la estructura del tipo Auxiliar1 es {0}", auxiliar1.GetHoras());
+
+            Dias dia_arrivo = Dias.Viernes;
+            Console.WriteLine("El profesor Juan llegara al pais desde madrid el dia {0}", dia_arrivo);
+            //string dia_partida = Dias.Miercoles;  esto no se puede porque enum no es string son constantes
+
+            //Si por alguna razon se necesite almacenar en un tipo enumerado un valor nulo se hace de la siguiente manera
+            Dias? valor_nulo = null;
+            Console.WriteLine("El contenido del enumerado valor_nullo es {0}", valor_nulo);
+
+            //para utilizar el valor numerico del enumerado tenemos que realizar un casting
+            int glucosa = (int)NivelGlucosa.Medio * 2;
+            Console.WriteLine("El nivel de glucosa es de {0}", glucosa);
 
         }
     }
 }
 
-public class Auxiliar 
-{
-    private int horasTrabajadas;
-    private int diasTrabajados;
-    public Auxiliar() { }
-    public void SetHoras (int horas) =>  this.horasTrabajadas = horas;
-    public int GetHoras () => this.horasTrabajadas;
-    public void SetDias (int dias) => this.diasTrabajados = dias;
-    public int GetDias () => this.diasTrabajados;
 
-    public void IncrementarHoras(Auxiliar auxiliar, int _horas )
-    {
-        auxiliar.SetHoras( horasTrabajadas + _horas );
-    }
 
-}
-
-public struct Auxiliar1
-{
-    private int horasTrabajadas;
-    private int diasTrabajados;
-    public void Auxiliar() { }
-    public void SetHoras(int horas) => this.horasTrabajadas = horas;
-    public int GetHoras() => this.horasTrabajadas;
-    public void SetDias(int dias) => this.diasTrabajados = dias;
-    public int GetDias() => this.diasTrabajados;
-
-    public void IncrementarHoras(Auxiliar1 auxiliar1, int _horas)
-    {
-        auxiliar1.SetHoras(horasTrabajadas + _horas);
-    }
-}
 
 
 
