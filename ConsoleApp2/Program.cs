3161664c51ea016b19a80e86d682a6de49cd6056
baires1971 <baires1971@hotmail.com>
2022-04-06 22:39:32 -0300

    25 - Colecciones genericas

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-
-
+using System.Collections.Generic;
 
 namespace ConsoleApp2
 {
@@ -9,30 +8,51 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            /* Clases genericas con restricciones sirven para manejar cualquier tipo de datos, pero restringiendo su uso para ciertas
-             * clases u objetos que nosotros queramos, por ejemplo si queremos crear un deposito que almacene solamente objetos de 
-             * clases que tengan relacion con una universidad, no debiendo almacenar clases que no tengan que ver con el ambito academico
-             * para ello a las clases de la universidad implementamos una interface especifica que no posean las demas clases
-             * 
-             * 
-             *  
+            /* Las Colecciones que pertenecen al namespace System.Collection.Generic, sirven para almacenar objetos
+             * son clases genericas, permiten ordenar, añadir, eliminar, buscar, etc.
              * 
            */
-            Deposito<Alumno> deposito = new Deposito<Alumno>();
-            Deposito<Profesor> deposito2 = new Deposito<Profesor>();
-            deposito.SetDato(new Alumno(100));
-            deposito2.SetDato(new Profesor(30));
-            //si queremos crear un deposito de la clase veterinario que no implementa la interface IUniversidad el compilador no nos permite
-            Deposito<Veterinario> deposito3 = new Deposito<Veterinario>();
+            // creamos una lista de enteros
+            List<int> listado = new List<int>();
+            listado.Add(1);  //agregamos el numero 1 en la primera posicion de la lista
+            listado.Add(2);
+            listado.Add(3);
+            for (int i = 0; i < listado.Count; i++) Console.WriteLine(listado[i]); 
+
+            Console.WriteLine("======================================================================");
+
+            LinkedList<int> lista = new LinkedList<int>();
+            lista.AddFirst(1);
+            lista.AddLast(3);  //agregamos el numero 3 en la ultima posicion de la linkedlist
+            lista.AddLast(4);
+            lista.AddLast(5);
+            lista.AddFirst(9);
+            LinkedListNode<int> unNodo = new LinkedListNode<int>(14); //creamos un nodo con el valor 14
+            lista.AddFirst(unNodo);  //agregamos el nodo creado en la primera posicion de la linkedlist
+            foreach (int i in lista) { Console.WriteLine(i); }
+            Console.ReadKey();
+            Console.WriteLine("======================================================================");
+            lista.Remove(5); //removemos el numero 5 de la linkedlist
+            for (LinkedListNode<int> nodo = lista.First; nodo != null; nodo = nodo.Next) 
+            {
+                int elemento = nodo.Value;
+                Console.WriteLine(elemento);
+            }
+            Console.WriteLine("======================================================================");
+            Queue<int> cola = new Queue<int>();
+            cola.Enqueue(5); //agregamos el numero 5 al final de la cola
+            cola.Enqueue(9);
+            cola.Enqueue(7);
+            foreach (int i in cola) { Console.WriteLine(i); }
+
+
+
         }
     }
         
 }
 
-interface IUniversidad
-{
-    int GetLegajo();
-}
+
 
 
 
